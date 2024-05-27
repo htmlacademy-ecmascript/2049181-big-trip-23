6784c8113f64862e1601d33f1a8af2303d5b9f57
Alexander Golubenko <webgolub@gmail.com>
2024-05-27 11:01:44 +0300

    refactor(module1-task3): Уберёт повторы кода в некоторых view

---
+++ src/const.js
@@ -0,0 +1,5 @@
+const EVENT_TYPES = ['Taxi', 'Bus', 'Train', 'Ship', 'Drive', 'Flight', 'Check-in', 'Sightseeing', 'Restaurant'];
+
+const FILTER_TYPES = ['Everything', 'Future', 'Present', 'Past'];
+
+export {EVENT_TYPES, FILTER_TYPES};
